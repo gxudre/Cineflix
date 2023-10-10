da0b78c59229ac127c69e0fbd216949acfb947f1
Gustavo Xudré <gxudre@gmail.com>
2023-10-10 16:58:11 -0300

    close #13 close #8

---
+++ src/screens/Home/style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    background-color: rgba(0,0,0,0.7);
+    flex:1;
+    justify-content: center;
+    align-items: center;
+`;
---
+++ src/screens/Login/index.js
@@ -0,0 +1,58 @@
+import React, { useEffect } from "react";
+import {
+  Container,
+  ImageBackground,
+  Input,
+  Logo,
+  ContainerInput,
+  Buttom,
+  Text,
+  TextContainer,
+  Form,
+} from "./style";
+import logo from "../../assets/logo.png";
+import imageBackground from "../../assets/login.jpg";
+
+const Login = ({navigation}) => {
+
+const handleLogin = () => {
+    navigation.replace("Home")
+}
+
+const handleEsqueceuSenha = () => {
+    alert('Esqueceu nada, só tem um login na aplicação!');
+}
+
+const handleCriarConta = () => {
+    alert('criar conta');
+}
+
+  return (
+    <ImageBackground source={imageBackground}>
+      <Container>
+        <Logo source={logo} />
+
+        <Form>
+          <ContainerInput>
+            <Input placeholder="E-mail" placeholderTextColor={"#000"} />
+          </ContainerInput>
+          <ContainerInput>
+            <Input placeholder="Senha" placeholderTextColor={"#000"} secureTextEntry={true}/>
+          </ContainerInput>
+          <Buttom onPress={handleLogin}>
+            <Text>Entrar</Text>
+          </Buttom>
+
+          <TextContainer>
+            <Text onPress={handleEsqueceuSenha}>Esqueceu a Senha?</Text>
+          </TextContainer>
+          <TextContainer>
+            <Text onPress={handleCriarConta}>Criar Conta</Text>
+          </TextContainer>
+        </Form>
+      </Container>
+    </ImageBackground>
+  );
+};
+
+export default Login;
